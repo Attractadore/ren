30016777685cda996e3adb6f4195b7023368f57c
Michael <attractadore02@gmail.com>
2022-12-06 01:52:23 +0300

    [DirectX12]: Implement swapchain

--- lib/DirectX12/DirectX12Swapchain.cpp
+++ lib/DirectX12/DirectX12Swapchain.cpp
@@ -1,13 +1,17 @@
 #include "DirectX12/DirectX12Swapchain.hpp"
+#include "DirectX12/DXGIFormat.hpp"
+#include "DirectX12/DirectX12CommandAllocator.hpp"
+#include "DirectX12/DirectX12Texture.hpp"
 #include "DirectX12/Errors.hpp"
 
 namespace ren {
 DirectX12Swapchain::DirectX12Swapchain(IDXGIFactory2 *factory,
                                        ID3D12CommandQueue *queue, HWND hwnd) {
+  m_hwnd = hwnd;
   DXGI_SWAP_CHAIN_DESC1 swapchain_desc = {
       .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
       .SampleDesc = {.Count = 1},
-      .BufferCount = 3,
+      .BufferCount = c_buffer_count,
       .SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD,
   };
   ComPtr<IDXGISwapChain1> swapchain;
@@ -15,9 +19,67 @@ DirectX12Swapchain::DirectX12Swapchain(IDXGIFactory2 *factory,
                                                 nullptr, nullptr, &swapchain),
                 "DXGI: Failed to create swapchain");
   throwIfFailed(swapchain.As(&m_swapchain), "DXGI: Failed to create swapchain");
-} // namespace ren
+  m_textures.resize(c_buffer_count);
+  setTextures();
+}
 
-void DirectX12Swapchain::setSize(unsigned width, unsigned height) {}
+void DirectX12Swapchain::setTextures() {
+  for (int i = 0; i < c_buffer_count; ++i) {
+    ComPtr<ID3D12Resource> surface;
+    m_swapchain->GetBuffer(i, IID_PPV_ARGS(&surface));
+    auto desc = surface->GetDesc();
+    m_textures[i] = {
+        .desc =
+            {
+                .type = TextureType::e2D,
+                .format = getFormat(desc.Format),
+                .usage = getTextureUsageFlags(desc.Flags),
+                .width = static_cast<unsigned>(desc.Width),
+                .height = desc.Height,
+                .layers = desc.DepthOrArraySize,
+                .levels = desc.MipLevels,
+            },
+        .handle = AnyRef(surface.Get(), [](void *) {}),
+    };
+  }
+}
 
-HWND DirectX12Swapchain::getHWND() const { DIRECTX12_UNIMPLEMENTED; }
+namespace {
+std::tuple<unsigned, unsigned> getWindowSize(HWND hwnd) {
+  RECT rect;
+  throwIfFailed(!!GetClientRect(hwnd, &rect),
+                "WIN32: Failed to get window size");
+  return {rect.right, rect.bottom};
+}
+
+std::tuple<unsigned, unsigned> getSwapchainSize(IDXGISwapChain1 *swapchain) {
+  DXGI_SWAP_CHAIN_DESC1 desc;
+  throwIfFailed(swapchain->GetDesc1(&desc),
+                "DXGI: Failed to get swapchain description");
+  return {desc.Width, desc.Height};
+}
+} // namespace
+
+void DirectX12Swapchain::AcquireBuffer(DirectX12CommandAllocator &cmd_alloc) {
+  // If the swapchain's window is minimized, don't do anything
+  if (IsIconic(m_hwnd)) {
+    return;
+  }
+  auto window_size = getWindowSize(m_hwnd);
+  auto swapchain_size = getSwapchainSize(m_swapchain.Get());
+  if (window_size != swapchain_size) {
+    // All accesses to the swapchain's buffers must be completed and all
+    // references to them must be released.
+    cmd_alloc.flush();
+    throwIfFailed(m_swapchain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, 0),
+                  "DXGI: Failed to resize swapchain");
+    setTextures();
+    // NOTE: swapchain size might still not match window size
+  }
+}
+
+void DirectX12Swapchain::PresentBuffer() {
+  throwIfFailed(m_swapchain->Present(1, 0),
+                "DXGI: Failed to present swapchain buffer");
+}
 } // namespace ren
--- lib/include/DirectX12/DirectX12Swapchain.hpp
+++ lib/include/DirectX12/DirectX12Swapchain.hpp
@@ -1,20 +1,37 @@
 #pragma once
 #include "Support/ComPtr.hpp"
+#include "Support/Vector.hpp"
 #include "Swapchain.hpp"
+#include "Texture.hpp"
 
 #include <d3d12.h>
 #include <dxgi1_4.h>
 
 namespace ren {
+class DirectX12CommandAllocator;
+
 class DirectX12Swapchain final : public Swapchain {
+  static constexpr auto c_buffer_count = 3;
+
+  HWND m_hwnd;
   ComPtr<IDXGISwapChain3> m_swapchain;
+  SmallVector<Texture, 3> m_textures;
+
+private:
+  void setTextures();
 
 public:
   DirectX12Swapchain(IDXGIFactory2 *factory, ID3D12CommandQueue *queue,
                      HWND hwnd);
 
-  void setSize(unsigned width, unsigned height) override;
+  void setSize(unsigned width, unsigned height) override {}
 
-  HWND getHWND() const;
+  HWND getHWND() const { return m_hwnd; }
+
+  void AcquireBuffer(DirectX12CommandAllocator &cmd_alloc);
+  const Texture &getTexture() {
+    return m_textures[m_swapchain->GetCurrentBackBufferIndex()];
+  }
+  void PresentBuffer();
 };
 } // namespace ren
