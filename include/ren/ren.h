c97c7affbfa545b5e70008defb7769f08f42c313
Michael Bargatin <attractadore02@gmail.com>
2022-12-25 22:28:50 +0300

    Update public API

--- include/ren/ren.h
+++ include/ren/ren.h
@@ -6,29 +6,91 @@ extern "C" {
 #endif
 
 typedef struct RenDevice RenDevice;
-typedef struct RenScene RenScene;
 typedef struct RenSwapchain RenSwapchain;
+typedef struct RenScene RenScene;
+typedef uint32_t RenMesh;
+typedef uint32_t RenMaterial;
+typedef uint32_t RenModel;
 
 void ren_DestroyDevice(RenDevice *device);
-
-RenScene *ren_CreateScene(RenDevice *device);
-void ren_DestroyScene(RenScene *scene);
-
-void ren_SetSceneOutputSize(RenScene *scene, unsigned width, unsigned height);
-unsigned ren_GetSceneOutputWidth(const RenScene *scene);
-unsigned ren_GetSceneOutputHeight(const RenScene *scene);
-
-void ren_DrawScene(RenScene *scene);
-
-void ren_SetSceneSwapchain(RenScene *scene, RenSwapchain *swapchain);
-RenSwapchain *ren_GetSceneSwapchain(const RenScene *scene);
 
 void ren_DestroySwapchain(RenSwapchain *swapchain);
 
 void ren_SetSwapchainSize(RenSwapchain *swapchain, unsigned width,
                           unsigned height);
-unsigned ren_GetSwapchainWidth(const RenSwapchain *swapchain);
-unsigned ren_GetSwapchainHeight(const RenSwapchain *swapchain);
+
+RenScene *ren_CreateScene(RenDevice *device);
+void ren_DestroyScene(RenScene *scene);
+
+void ren_SetSceneOutputSize(RenScene *scene, unsigned width, unsigned height);
+
+void ren_DrawScene(RenScene *scene);
+
+void ren_SetSceneSwapchain(RenScene *scene, RenSwapchain *swapchain);
+
+typedef enum {
+  REN_PROJECTION_PERSPECTIVE = 0,
+  REN_PROJECTION_ORTHOGRAPHIC,
+} RenProjection;
+
+typedef struct {
+  float hfov;
+} RenPerspectiveCameraDesc;
+
+typedef struct {
+  float width;
+} RenOrthographicCameraDesc;
+
+typedef struct {
+  RenProjection type;
+  union {
+    RenPerspectiveCameraDesc perspective;
+    RenOrthographicCameraDesc orthographic;
+  };
+  float aspect_ratio;
+  float position[3];
+  float forward[3];
+  float up[3];
+} RenCameraDesc;
+
+void ren_SetSceneCamera(RenScene *scene, const RenCameraDesc *desc);
+
+typedef struct {
+  unsigned num_vertices;
+  unsigned num_indices;
+  const float *positions;
+  const float *colors;
+  const unsigned *indices;
+} RenMeshDesc;
+
+RenMesh ren_CreateMesh(RenScene *scene, const RenMeshDesc *desc);
+void ren_DestroyMesh(RenScene *scene, RenMesh mesh);
+
+typedef enum {
+  REN_MATERIAL_ALBEDO_CONST,
+  REN_MATERIAL_ALBEDO_VERTEX,
+} RenMaterialAlbedo;
+
+typedef struct {
+  RenMaterialAlbedo albedo_type;
+  union {
+    float albedo_color[3];
+  };
+} RenMaterialDesc;
+
+RenMaterial ren_CreateMaterial(RenScene *scene, const RenMaterialDesc *desc);
+void ren_DestroyMaterial(RenScene *scene, RenMaterial material);
+
+typedef struct {
+  RenMesh mesh;
+  RenMaterial material;
+} RenModelDesc;
+
+RenModel ren_CreateModel(RenScene *scene, const RenModelDesc *desc);
+void ren_DestroyModel(RenScene *scene, RenModel model);
+
+void ren_SetModelMatrix(RenScene *scene, RenModel model,
+                        const float matrix[16]);
 
 #ifdef __cplusplus
 }
--- include/ren/ren.hpp
+++ include/ren/ren.hpp
@@ -1,7 +1,14 @@
 #pragma once
 #include "ren.h"
 
+#include <glm/vec3.hpp>
+
+#include <cassert>
+#include <concepts>
+#include <cstring>
 #include <memory>
+#include <span>
+#include <variant>
 
 struct RenDevice {
   ~RenDevice() = delete;
@@ -17,6 +24,77 @@ struct RenScene {
 
 namespace ren {
 inline namespace v0 {
+
+struct Scene;
+
+namespace detail {
+
+template <typename T>
+concept scoped_enum =
+    std::is_enum_v<T> and not std::convertible_to<T, std::underlying_type_t<T>>;
+
+template <scoped_enum H> class SharedHandle;
+
+template <typename H> constexpr void (Scene::*HandleDestroy)(H);
+
+template <typename H> constexpr void *from_handle(H handle) {
+  return reinterpret_cast<void *>(static_cast<uintptr_t>(handle));
+}
+
+template <typename H> constexpr H to_handle(void *handle) {
+  return static_cast<H>(reinterpret_cast<uintptr_t>(handle));
+}
+
+template <typename H> struct HandleDeleter {
+  Scene *scene = nullptr;
+
+  void operator()(void *handle) {
+    if (handle) {
+      assert(scene);
+      (scene->*HandleDestroy<H>)(to_handle<H>(handle));
+    }
+  }
+};
+
+template <scoped_enum H> class UniqueHandle {
+  friend class SharedHandle<H>;
+
+  std::unique_ptr<void, HandleDeleter<H>> m_holder;
+
+public:
+  UniqueHandle() = default;
+  UniqueHandle(Scene *scene, H handle)
+      : m_holder(from_handle(handle), HandleDeleter<H>{scene}) {}
+
+  H get() const { return to_handle<H>(m_holder.get()); }
+
+  const Scene *get_scene() const { return m_holder.get_deleter().scene; }
+
+  Scene *get_scene() { return m_holder.get_deleter().scene; }
+};
+
+template <scoped_enum H> class SharedHandle {
+  std::shared_ptr<void> m_holder;
+
+public:
+  SharedHandle(UniqueHandle<H> unique) : m_holder(std::move(unique.m_holder)) {}
+
+  H get() const { reinterpret_cast<H>(m_holder.get()); }
+
+  const Scene *get_scene() const {
+    return get_deleter<HandleDeleter<H>>(m_holder)->scene;
+  }
+
+  Scene *get_scene() { return get_deleter<HandleDeleter<H>>(m_holder)->scene; }
+};
+
+template <class... Ts> struct overload_set : Ts... {
+  overload_set(Ts... fs) : Ts(std::move(fs))... {}
+  using Ts::operator()...;
+};
+
+} // namespace detail
+
 struct Device;
 struct DeviceDeleter {
   void operator()(Device *device) const noexcept {
@@ -35,7 +113,6 @@ struct SwapchainDeleter {
 using UniqueSwapchain = std::unique_ptr<Swapchain, SwapchainDeleter>;
 using SharedSwapchain = std::shared_ptr<Swapchain>;
 
-struct Scene;
 struct SceneDeleter {
   void operator()(Scene *scene) const noexcept {
     ren_DestroyScene(reinterpret_cast<RenScene *>(scene));
@@ -43,6 +120,18 @@ struct SceneDeleter {
 };
 using UniqueScene = std::unique_ptr<Scene, SceneDeleter>;
 using SharedScene = std::shared_ptr<Scene>;
+
+enum class Mesh : RenMesh;
+using UniqueMesh = detail::UniqueHandle<Mesh>;
+using SharedMesh = detail::SharedHandle<Mesh>;
+
+enum class Material : RenMaterial;
+using UniqueMaterial = detail::UniqueHandle<Material>;
+using SharedMaterial = detail::SharedHandle<Material>;
+
+enum class Model : RenModel;
+using UniqueModel = detail::UniqueHandle<Model>;
+using SharedModel = detail::SharedHandle<Model>;
 
 struct Device : RenDevice {
   UniqueScene create_scene() {
@@ -54,23 +143,69 @@ struct Swapchain : RenSwapchain {
   void set_size(unsigned width, unsigned height) {
     ren_SetSwapchainSize(this, width, height);
   }
+};
 
-  std::pair<unsigned, unsigned> get_size() const {
-    return {ren_GetSwapchainWidth(this), ren_GetSwapchainHeight(this)};
-  }
+using PerspectiveCameraDesc = RenPerspectiveCameraDesc;
+using OrthographicCameraDesc = RenOrthographicCameraDesc;
+
+struct CameraDesc {
+  std::variant<PerspectiveCameraDesc, OrthographicCameraDesc> projection_desc;
+  float aspect_ratio;
+  glm::vec3 position;
+  glm::vec3 forward;
+  glm::vec3 up;
 };
 
 struct CameraRef {
-  RenScene *m_scene;
+  RenScene *m_scene = nullptr;
 
 public:
-  CameraRef() : CameraRef(nullptr) {}
+  CameraRef() = default;
   CameraRef(Scene *scene) : m_scene(reinterpret_cast<RenScene *>(scene)) {}
 
   Scene *get_scene() { return reinterpret_cast<Scene *>(m_scene); };
   const Scene *get_scene() const {
     return reinterpret_cast<const Scene *>(m_scene);
   };
+
+  void config(const CameraDesc &desc) {
+    RenCameraDesc c_desc = {.aspect_ratio = desc.aspect_ratio};
+    std::visit(
+        detail::overload_set{[&](const PerspectiveCameraDesc &perspective) {
+                               c_desc.type = REN_PROJECTION_PERSPECTIVE;
+                               c_desc.perspective = perspective;
+                             },
+                             [&](const OrthographicCameraDesc &ortho) {
+                               c_desc.type = REN_PROJECTION_ORTHOGRAPHIC;
+                               c_desc.orthographic = ortho;
+                             }},
+        desc.projection_desc);
+    std::memcpy(c_desc.position, &desc.position, sizeof(desc.position));
+    std::memcpy(c_desc.forward, &desc.forward, sizeof(desc.forward));
+    std::memcpy(c_desc.up, &desc.up, sizeof(desc.up));
+    ren_SetSceneCamera(m_scene, &c_desc);
+  }
+};
+
+struct MeshDesc {
+  std::span<const glm::vec3> positions;
+  std::span<const glm::vec3> colors;
+  std::span<const unsigned> indices;
+};
+
+struct ConstMaterialAlbedo {
+  glm::vec3 color;
+};
+
+struct VertexMaterialAlbedo {};
+
+struct MaterialDesc {
+  std::variant<ConstMaterialAlbedo, VertexMaterialAlbedo> albedo;
+};
+
+struct ModelDesc {
+  Mesh mesh;
+  Material material;
 };
 
 struct Scene : RenScene {
@@ -84,11 +219,97 @@ struct Scene : RenScene {
     ren_SetSceneOutputSize(this, width, height);
   }
 
-  std::pair<unsigned, unsigned> get_output_size() const {
-    return {ren_GetSceneOutputWidth(this), ren_GetSceneOutputHeight(this)};
+  void draw() { ren_DrawScene(this); }
+
+  Mesh create_mesh(const MeshDesc &desc) {
+    assert(not desc.positions.empty());
+    assert(desc.colors.empty() or desc.colors.size() == desc.positions.size());
+    assert(not desc.indices.empty());
+    RenMeshDesc c_desc = {
+        .num_vertices = unsigned(desc.positions.size()),
+        .num_indices = unsigned(desc.indices.size()),
+        .positions = reinterpret_cast<const float *>(desc.positions.data()),
+        .colors = desc.colors.empty()
+                      ? nullptr
+                      : reinterpret_cast<const float *>(desc.colors.data()),
+        .indices = desc.indices.data(),
+    };
+    return static_cast<Mesh>(ren_CreateMesh(this, &c_desc));
   }
 
-  void draw() { ren_DrawScene(this); }
+  void destroy_mesh(Mesh mesh) {
+    ren_DestroyMesh(this, static_cast<RenMesh>(mesh));
+  }
+
+  UniqueMesh create_unique_mesh(const MeshDesc &desc) {
+    return {this, create_mesh(desc)};
+  }
+
+  SharedMesh create_shared_mesh(const MeshDesc &desc) {
+    return create_unique_mesh(desc);
+  }
+
+  Material create_material(const MaterialDesc &desc) {
+    RenMaterialDesc c_desc = {};
+    std::visit(
+        detail::overload_set{[&](const ConstMaterialAlbedo &albedo) {
+                               c_desc.albedo_type = REN_MATERIAL_ALBEDO_CONST;
+                               std::memcpy(&c_desc.albedo_color, &albedo.color,
+                                           sizeof(albedo.color));
+                             },
+                             [&](const VertexMaterialAlbedo &albedo) {
+                               c_desc.albedo_type = REN_MATERIAL_ALBEDO_VERTEX;
+                             }},
+        desc.albedo);
+    return static_cast<Material>(ren_CreateMaterial(this, &c_desc));
+  }
+
+  void destroy_material(Material material) {
+    ren_DestroyMaterial(this, static_cast<RenMaterial>(material));
+  }
+
+  UniqueMaterial create_unique_material(const MaterialDesc &desc) {
+    return {this, create_material(desc)};
+  }
+
+  SharedMaterial create_shared_material(const MaterialDesc &desc) {
+    return create_unique_material(desc);
+  }
+
+  Model create_model(const ModelDesc &desc) {
+    RenModelDesc c_desc = {
+        .mesh = static_cast<RenMesh>(desc.mesh),
+        .material = static_cast<RenMaterial>(desc.material),
+    };
+    return static_cast<Model>(ren_CreateModel(this, &c_desc));
+  }
+
+  void destroy_model(Model model) {
+    ren_DestroyModel(this, static_cast<RenModel>(model));
+  }
+
+  UniqueModel create_unique_model(const ModelDesc &desc) {
+    return {this, create_model(desc)};
+  }
+
+  SharedModel create_shared_model(const ModelDesc &desc) {
+    return create_unique_model(desc);
+  }
 };
+
+namespace detail {
+
+template <>
+inline constexpr void (Scene::*HandleDestroy<Mesh>)(Mesh) =
+    &Scene::destroy_mesh;
+template <>
+inline constexpr void (Scene::*HandleDestroy<Material>)(Material) =
+    &Scene::destroy_material;
+template <>
+inline constexpr void (Scene::*HandleDestroy<Model>)(Model) =
+    &Scene::destroy_model;
+
+} // namespace detail
+
 } // namespace v0
 } // namespace ren
