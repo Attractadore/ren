64862b22a8f2f5ad4104715887f2dd3951e09c57
Michael Bargatin <attractadore02@gmail.com>
2023-01-07 02:01:11 +0300

    Implement DescriptorSetAllocator

--- lib/include/DescriptorSetAllocator.hpp
+++ lib/include/DescriptorSetAllocator.hpp
@@ -1,10 +1,26 @@
 #pragma once
+#include "Config.hpp"
 #include "Def.hpp"
 #include "Descriptors.hpp"
+#include "Support/FreeListAllocator.hpp"
 
 namespace ren {
 
 class DescriptorSetAllocator {
+  Device *m_device;
+
+  struct FrameDescriptorSetAllocator {
+    Vector<DescriptorPool> pools;
+    unsigned num_used;
+  };
+
+  unsigned m_frame_index = 0;
+  std::array<FrameDescriptorSetAllocator, c_pipeline_depth> m_frame_pools;
+
+  auto get_frame_allocator() -> FrameDescriptorSetAllocator & {
+    return m_frame_pools[m_frame_index];
+  }
+
 public:
   DescriptorSetAllocator(Device &device);
 
