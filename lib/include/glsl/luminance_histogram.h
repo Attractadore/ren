2d027409d74fce523dfca31086ce540abbf07d23
Michael Bargatin <attractadore02@gmail.com>
2023-06-04 14:31:05 +0300

    Adjust luminance histogram range

--- lib/include/glsl/luminance_histogram.h
+++ lib/include/glsl/luminance_histogram.h
@@ -5,9 +5,14 @@
 
 REN_NAMESPACE_BEGIN
 
-const float MIN_LUMINANCE = 1.0f / 65536.0f;
+// Absolute threshold of vision is 1e-6 cd/m^2
+const float MIN_LUMINANCE = 1.0e-7f;
+// Eye damage is possible at 1e8 cd/m^2
+const float MAX_LUMINANCE = 1.0e9f;
+
 const float MIN_LOG_LUMINANCE = log2(MIN_LUMINANCE);
-const float MAX_LOG_LUMINANCE = 16.0f;
+const float MAX_LOG_LUMINANCE = log2(MAX_LUMINANCE);
+
 const uint NUM_LUMINANCE_HISTOGRAM_BINS = 64;
 
 REN_BUFFER_REFERENCE(4) LuminanceHistogram {
