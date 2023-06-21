21e6020aced609cf594b949e2f132523836b4e85
Michael Bargatin <attractadore02@gmail.com>
2023-06-22 00:46:09 +0300

    [Support]: Update HashMap API

--- lib/include/Support/HashMap.hpp
+++ lib/include/Support/HashMap.hpp
@@ -1,10 +1,38 @@
 #pragma once
-#include <unordered_map>
-
 #include "Hash.hpp"
+#include "Optional.hpp"
+
+#include <unordered_map>
 
 namespace ren {
 
 template <typename K, typename V>
-using HashMap = std::unordered_map<K, V, Hash<K>>;
-}
+struct HashMap : std::unordered_map<K, V, Hash<K>> {
+  using Base = std::unordered_map<K, V, Hash<K>>;
+  using Base::Base;
+
+  using Base::insert;
+  auto insert(K key, V value) -> Optional<V &> {
+    auto [it, inserted] = insert(std::pair(std::move(key), std::move(value)));
+    if (inserted) {
+      return it->second;
+    }
+    return None;
+  }
+
+  using Base::operator[];
+  auto operator[](const K &key) const -> const V & {
+    auto it = this->find(key);
+    assert(it != this->end());
+    return it->second;
+  }
+
+  auto erase_if(const std::predicate<const K &, const V &> auto &pred)
+      -> usize {
+    return std::erase_if(*this, [&](const std::pair<K, V> &kv) {
+      return pred(kv.first, kv.second);
+    });
+  }
+};
+
+} // namespace ren
