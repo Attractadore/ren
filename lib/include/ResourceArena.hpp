cf79fc49ecc99dbe2ff8e8f9ad317252f798ca3d
Michael Bargatin <attractadore02@gmail.com>
2023-06-22 05:53:47 +0300

    Use HashSets instead of Vectors for ResourceArena handles
    This will make clearing the arena faster and memory usage lower at
    the cost of slower handle destruction

--- lib/include/ResourceArena.hpp
+++ lib/include/ResourceArena.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "Buffer.hpp"
 #include "Device.hpp"
+#include "Support/HashSet.hpp"
 
 namespace ren {
 
@@ -8,47 +9,57 @@ namespace detail {
 
 template <typename... Ts> class ResourceArenaImpl {
   Device *m_device;
-  std::tuple<Vector<Handle<Ts>>...> m_resources;
+  std::tuple<HashSet<Handle<Ts>>...> m_resources;
 
 private:
-  template <typename T> auto get_type_arena() -> Vector<Handle<T>> & {
-    return std::get<Vector<Handle<T>>>(m_resources);
+  template <typename T> auto get_type_arena() -> HashSet<Handle<T>> & {
+    return std::get<HashSet<Handle<T>>>(m_resources);
   }
 
-  template <typename T> void push_back(Handle<T> handle) {
-    get_type_arena<T>().push_back(handle);
+  template <typename T> void insert(Handle<T> handle) {
+    get_type_arena<T>().insert(handle);
   }
 
-  void destroy(Handle<Buffer> buffer) { destroy_buffer(buffer); }
-
-  void destroy(Handle<Texture> texture) { destroy_texture(texture); }
-
-  void destroy(Handle<Sampler> sampler) { destroy_sampler(sampler); }
-
-  void destroy(Handle<Semaphore> semaphore) { destroy_semaphore(semaphore); }
-
-  void destroy(Handle<DescriptorPool> pool) { destroy_descriptor_pool(pool); }
-
-  void destroy(Handle<DescriptorSetLayout> layout) {
-    destroy_descriptor_set_layout(layout);
+  void destroy_handle(Handle<Buffer> buffer) {
+    m_device->destroy_buffer(buffer);
   }
 
-  void destroy(Handle<PipelineLayout> layout) {
-    destroy_pipeline_layout(layout);
+  void destroy_handle(Handle<Texture> texture) {
+    m_device->destroy_texture(texture);
   }
 
-  void destroy(Handle<GraphicsPipeline> pipeline) {
-    destroy_graphics_pipeline(pipeline);
+  void destroy_handle(Handle<Sampler> sampler) {
+    m_device->destroy_sampler(sampler);
   }
 
-  void destroy(Handle<ComputePipeline> pipeline) {
-    destroy_compute_pipeline(pipeline);
+  void destroy_handle(Handle<Semaphore> semaphore) {
+    m_device->destroy_semaphore(semaphore);
+  }
+
+  void destroy_handle(Handle<DescriptorPool> pool) {
+    m_device->destroy_descriptor_pool(pool);
+  }
+
+  void destroy_handle(Handle<DescriptorSetLayout> layout) {
+    m_device->destroy_descriptor_set_layout(layout);
+  }
+
+  void destroy_handle(Handle<PipelineLayout> layout) {
+    m_device->destroy_pipeline_layout(layout);
+  }
+
+  void destroy_handle(Handle<GraphicsPipeline> pipeline) {
+    m_device->destroy_graphics_pipeline(pipeline);
+  }
+
+  void destroy_handle(Handle<ComputePipeline> pipeline) {
+    m_device->destroy_compute_pipeline(pipeline);
   }
 
   template <typename T> void clear() {
     auto &handles = get_type_arena<T>();
     for (auto handle : handles) {
-      destroy(handle);
+      destroy_handle(handle);
     }
     handles.clear();
   }
@@ -67,100 +78,95 @@ public:
 
   auto create_buffer(const BufferCreateInfo &&create_info) -> Handle<Buffer> {
     auto buffer = m_device->create_buffer(std::move(create_info));
-    push_back(buffer);
+    insert(buffer);
     return buffer;
   }
 
-  void destroy_buffer(Handle<Buffer> buffer) {
-    m_device->destroy_buffer(buffer);
-  }
+  void destroy_buffer(Handle<Buffer> buffer) { destroy(buffer); }
 
   auto create_texture(const TextureCreateInfo &&create_info)
       -> Handle<Texture> {
     auto texture = m_device->create_texture(std::move(create_info));
-    push_back(texture);
+    insert(texture);
     return texture;
   }
 
-  void destroy_texture(Handle<Texture> texture) {
-    m_device->destroy_texture(texture);
-  }
+  void destroy_texture(Handle<Texture> texture) { destroy(texture); }
 
   auto create_sampler(const SamplerCreateInfo &&create_info)
       -> Handle<Sampler> {
     auto sampler = m_device->create_sampler(std::move(create_info));
-    push_back(sampler);
+    insert(sampler);
     return sampler;
   }
 
-  void destroy_sampler(Handle<Sampler> sampler) {
-    m_device->destroy_sampler(sampler);
-  }
+  void destroy_sampler(Handle<Sampler> sampler) { destroy(sampler); }
 
   auto create_semaphore(const SemaphoreCreateInfo &&create_info)
       -> Handle<Semaphore> {
     auto semaphore = m_device->create_semaphore(std::move(create_info));
-    push_back(semaphore);
+    insert(semaphore);
     return semaphore;
   }
 
-  void destroy_semaphore(Handle<Semaphore> semaphore) {
-    m_device->destroy_semaphore(semaphore);
-  }
+  void destroy_semaphore(Handle<Semaphore> semaphore) { destroy(semaphore); }
 
   auto create_descriptor_pool(const DescriptorPoolCreateInfo &&create_info) {
     auto pool = m_device->create_descriptor_pool(std::move(create_info));
-    push_back(pool);
+    insert(pool);
     return pool;
   }
 
-  void destroy_descriptor_pool(Handle<DescriptorPool> pool) {
-    m_device->destroy_descriptor_pool(pool);
-  }
+  void destroy_descriptor_pool(Handle<DescriptorPool> pool) { destroy(pool); }
 
   auto create_descriptor_set_layout(
       const DescriptorSetLayoutCreateInfo &&create_info) {
     auto layout =
         m_device->create_descriptor_set_layout(std::move(create_info));
-    push_back(layout);
+    insert(layout);
     return layout;
   }
 
   void destroy_descriptor_set_layout(Handle<DescriptorSetLayout> layout) {
-    m_device->destroy_descriptor_set_layout(layout);
+    destroy(layout);
   }
 
   auto create_pipeline_layout(const PipelineLayoutCreateInfo &&create_info)
       -> Handle<PipelineLayout> {
     auto layout = m_device->create_pipeline_layout(std::move(create_info));
-    push_back(layout);
+    insert(layout);
     return layout;
   }
 
   void destroy_pipeline_layout(Handle<PipelineLayout> layout) {
-    m_device->destroy_pipeline_layout(layout);
+    destroy(layout);
   }
 
   auto create_graphics_pipeline(const GraphicsPipelineCreateInfo &&create_info)
       -> Handle<GraphicsPipeline> {
     auto pipeline = m_device->create_graphics_pipeline(std::move(create_info));
-    push_back(pipeline);
+    insert(pipeline);
     return pipeline;
   }
 
   void destroy_graphics_pipeline(Handle<GraphicsPipeline> pipeline) {
-    m_device->destroy_graphics_pipeline(pipeline);
+    destroy(pipeline);
   }
 
   auto create_compute_pipeline(const ComputePipelineCreateInfo &&create_info)
       -> Handle<ComputePipeline> {
     auto pipeline = m_device->create_compute_pipeline(std::move(create_info));
-    push_back(pipeline);
+    insert(pipeline);
     return pipeline;
   }
 
   void destroy_compute_pipeline(Handle<ComputePipeline> pipeline) {
-    m_device->destroy_compute_pipeline(pipeline);
+    destroy(pipeline);
+  }
+
+  template <typename T> void destroy(Handle<T> handle) {
+    destroy_handle(handle);
+    get_type_arena<T>().erase(handle);
   }
 
   void clear() { (clear<Ts>(), ...); }
