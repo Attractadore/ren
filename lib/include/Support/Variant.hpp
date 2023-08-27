e7ae7e954aa9a3ccac867f2055dc56b752b716b2
Michael Bargatin <attractadore02@gmail.com>
2023-08-28 00:57:21 +0200

    [Support]: improve Variant

--- lib/include/Support/Variant.hpp
+++ lib/include/Support/Variant.hpp
@@ -9,9 +9,20 @@ template <class... Ts> struct OverloadSet : Ts... {
   using Ts::operator()...;
 };
 
+namespace detail {
+
+template <typename T, typename... Ts>
+concept CSameAsAnyOf = (std::same_as<T, Ts> or ...);
+
+};
+
+using Monostate = std::monostate;
+
 template <typename... Ts> struct Variant : std::variant<Ts...> {
 public:
   using std::variant<Ts...>::variant;
+
+#define CVariantType(U) detail::CSameAsAnyOf<U, Ts...>
 
   template <typename... Fs> auto visit(Fs &&...funcs) const {
     return std::visit(OverloadSet{std::forward<Fs>(funcs)...}, *this);
@@ -21,19 +32,40 @@ public:
     return std::visit(OverloadSet{std::forward<Fs>(funcs)...}, *this);
   }
 
-  template <typename T> auto get() const -> Optional<const T &> {
+  template <typename T>
+    requires CVariantType(T)
+  auto get() const -> Optional<const T &> {
     if (const auto *ptr = std::get_if<T>(this)) {
       return *ptr;
     }
     return None;
   }
 
-  template <typename T> auto get() -> Optional<T &> {
+  template <typename T>
+    requires CVariantType(T)
+  auto get() -> Optional<T &> {
     if (auto *ptr = std::get_if<T>(this)) {
       return *ptr;
     }
     return None;
   }
+
+  template <typename T>
+    requires CVariantType(T)
+  auto get_or_emplace() -> T & {
+    if (auto *ptr = std::get_if<T>(this)) {
+      return *ptr;
+    }
+    return this->template emplace<T>();
+  }
+
+  explicit operator bool() const
+    requires CVariantType(Monostate)
+  {
+    return not std::holds_alternative<Monostate>(*this);
+  }
+
+#undef CVariantType
 };
 
 } // namespace ren
