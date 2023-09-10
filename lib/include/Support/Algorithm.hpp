03b1867c8a0bbffe338453c6923cfb14af08845e
Michael Bargatin <attractadore02@gmail.com>
2023-09-10 22:00:01 +0200

    [Support]: add rotate_right algorithm

--- lib/include/Support/Algorithm.hpp
+++ lib/include/Support/Algorithm.hpp
@@ -9,4 +9,13 @@ auto rotate_left(R &&r) -> ranges::borrowed_subrange_t<R> {
   return ranges::rotate(std::forward<R>(r), ranges::next(ranges::begin(r)));
 }
 
+template <ranges::random_access_range R>
+  requires ranges::permutable<ranges::iterator_t<R>>
+auto rotate_right(R &&r) -> ranges::borrowed_subrange_t<R> {
+  if (not ranges::empty(r)) {
+    return ranges::rotate(std::forward<R>(r), ranges::prev(ranges::end(r)));
+  }
+  return {ranges::end(r), ranges::end(r)};
+}
+
 } // namespace ren
