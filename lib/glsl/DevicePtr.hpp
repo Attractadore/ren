53f9fa3b57d740ff8e5c50ec9285c257b7531215
Michael Bargatin <attractadore02@gmail.com>
2024-06-12 00:38:56 +0200

    Add offseting operators to DevicePtr

--- lib/glsl/DevicePtr.hpp
+++ lib/glsl/DevicePtr.hpp
@@ -5,20 +5,38 @@
 namespace ren {
 
 template <typename T> class DevicePtr {
-  u64 m_value = 0;
-
 public:
   DevicePtr() = default;
   DevicePtr(std::nullptr_t) : DevicePtr() {}
-  explicit DevicePtr(u64 value) {
-    ren_assert_msg(value % alignof(T) == 0,
-                   "Device pointer improperly aligned");
-    m_value = value;
+  explicit DevicePtr(u64 ptr) {
+    ren_assert_msg(ptr % alignof(T) == 0,
+                   "Device pointer is improperly aligned");
+    m_ptr = ptr;
   }
 
-  bool is_null() const { return m_value == 0; }
+  bool is_null() const { return m_ptr == 0; }
 
   explicit operator bool() const { return !is_null(); }
+
+  auto operator+=(isize offset) -> DevicePtr & {
+    m_ptr += offset * sizeof(T);
+    return *this;
+  }
+
+  auto operator+(isize offset) const -> DevicePtr {
+    DevicePtr copy = *this;
+    copy += offset;
+    return copy;
+  }
+
+  auto operator-=(isize offset) -> DevicePtr & { return *this += -offset; }
+
+  auto operator-(isize offset) const -> DevicePtr {
+    return (*this) + (-offset);
+  }
+
+private:
+  u64 m_ptr = 0;
 };
 
 #define GLSL_PTR(Type) ren::DevicePtr<Type>
