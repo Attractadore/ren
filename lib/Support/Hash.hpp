5a3920101c35e0ca8ba931879140f14eddfeda1e
Michael Bargatin <attractadore02@gmail.com>
2024-06-11 22:18:13 +0200

    [Support]: fix hash_combine

--- lib/Support/Hash.hpp
+++ lib/Support/Hash.hpp
@@ -16,8 +16,8 @@ template <typename T>
   requires requires { std::hash<T>(); }
 struct Hash<T> : std::hash<T> {};
 
-auto hash_combine(usize hash, const auto &value) -> usize {
-  boost::hash_combine(hash, value);
+template <typename T> auto hash_combine(usize hash, const T &value) -> usize {
+  boost::hash_combine(hash, Hash<T>()(value));
   return hash;
 }
 
