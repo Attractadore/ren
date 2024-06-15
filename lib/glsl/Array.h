d4412eaffac5f2351bb0bdb6fac674a56c794534
Michael Bargatin <attractadore02@gmail.com>
2024-06-15 14:14:18 +0200

    [GLSL]: add array macro

---
+++ lib/glsl/Array.h
@@ -0,0 +1,16 @@
+#ifndef REN_GLSL_ARRAY_H
+#define REN_GLSL_ARRAY_H
+
+#ifdef GL_core_profile
+
+#define GLSL_ARRAY(T, name, SIZE) T name[SIZE]
+
+#else
+
+#include <array>
+
+#define GLSL_ARRAY(T, name, SIZE) std::array<T, SIZE> name
+
+#endif
+
+#endif // REN_GLSL_ARRAY_H
