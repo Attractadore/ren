30effaf6d96f3d28c1b2dc04e04ae3d37c55664e
Michael Bargatin <attractadore02@gmail.com>
2024-06-16 21:36:02 +0200

    [GLSL]: add in/out macros

--- lib/glsl/Common.h
+++ lib/glsl/Common.h
@@ -9,6 +9,10 @@
 #define GLSL_NAMESPACE_BEGIN
 #define GLSL_NAMESPACE_END
 
+#define GLSL_IN(T) T
+#define GLSL_OUT(T) out T
+#define GLSL_INOUT(T) inout T
+
 #else
 #include "Keywords.hpp"
 
@@ -17,6 +21,10 @@
 
 #define GLSL_NAMESPACE_BEGIN namespace ren::glsl {
 #define GLSL_NAMESPACE_END }
+
+#define GLSL_IN(T) const T&
+#define GLSL_OUT(T) T &
+#define GLSL_INOUT(T) T &
 
 GLSL_NAMESPACE_BEGIN
 using namespace glm;
