5772c5a22ad56fde878656ef6f195a10e2ddf564
Michael Bargatin <attractadore02@gmail.com>
2024-12-25 21:32:43 +0100

    [core]: add operator try macro

--- lib/core/Result.hpp
+++ lib/core/Result.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "Macros.hpp"
+
 #include <expected>
 
 namespace ren {
@@ -6,5 +8,14 @@ namespace ren {
 template <typename T, typename E> using Result = std::expected<T, E>;
 
 template <typename E> using Failed = std::unexpected<E>;
+
+#define ren_try_result ren_cat(res, __LINE__)
+
+#define ren_try_to(...)                                                        \
+  auto ren_try_result = (__VA_ARGS__);                                         \
+  static_assert(std::same_as<decltype(ren_try_result)::value_type, void>);     \
+  if (!ren_try_result) {                                                       \
+    return Failed(std::move(ren_try_result.error()));                          \
+  }
 
 } // namespace ren
