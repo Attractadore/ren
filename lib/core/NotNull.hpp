e81dd9bcb992df566c6cd1d5ec301397a9ca212b
Michael Bargatin <attractadore02@gmail.com>
2025-03-25 19:55:24 +0100

    Get rid of ms-gsl

--- lib/core/NotNull.hpp
+++ lib/core/NotNull.hpp
@@ -1,10 +1,32 @@
 #pragma once
-#include <gsl/pointers>
+#include <concepts>
+#include <type_traits>
+
+#include "Assert.hpp"
 
 namespace ren {
 
 template <typename P>
   requires std::is_pointer_v<P>
-using NotNull = gsl::not_null<P>;
+class NotNull {
+public:
+  NotNull(std::nullptr_t) = delete;
 
-}
+  template <std::convertible_to<P> A> NotNull(A ptr) {
+    ren_assert(ptr);
+    m_ptr = ptr;
+  }
+
+  auto &operator*() const { return *m_ptr; }
+
+  auto operator->() const -> P { return m_ptr; }
+
+  operator P() const { return m_ptr; }
+
+  auto get() const -> P { return m_ptr; }
+
+private:
+  P m_ptr = nullptr;
+};
+
+} // namespace ren
