17fb2c5599ae19faff34d358db6850e6a27fe1a3
Michael Bargatin <attractadore02@gmail.com>
2025-04-19 14:42:21 +0200

    Fix normal offset

--- lib/glsl/Transforms.h
+++ lib/glsl/Transforms.h
@@ -10,15 +10,16 @@ inline mat3 adjugate(mat3 m) {
 inline mat3 normal(mat3 m) { return adjugate(m); }
 
 inline vec2 pixel_view_space_size(float p00, float p11, vec2 size, float z) {
-  // s_ndc = a * s_v / z_v =>
-  // s_v = s_ndc * z_v / a
+  // s_ndc = a * s_v / -z_v =>
+  // s_v = s_ndc * -z_v / a
   // s_ndc = 2 * s_uv = 2 / size
-  return (2.0f * z) / (vec2(p00, p11) * size);
+  return (2.0f * -z) / (vec2(p00, p11) * size);
 }
 
 inline vec3 normal_offset(vec3 p, vec3 v, vec3 n, vec2 pixel_size) {
   float diag = length(pixel_size);
-  float sin_v = dot(v, n);
+  float cos_v = dot(v, n);
+  float sin_v = sqrt(1.0f - cos_v * cos_v);
   return p + (0.5f * sin_v * diag) * n;
 }
 
