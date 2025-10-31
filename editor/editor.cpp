9d5557ca6afdcfab3fc4b35b2ce99daaf4dc2242
Michael Bargatin <attractadore02@gmail.com>
2025-10-31 18:18:25 +0100

    [editor]: enable ImGui

--- editor/editor.cpp
+++ editor/editor.cpp
@@ -1,3 +1,4 @@
+#include "ren/core/Algorithm.hpp"
 #include "ren/core/Chrono.hpp"
 #include "ren/core/StdDef.hpp"
 #include "ren/ren.hpp"
@@ -6,16 +7,20 @@
 #include <cstdlib>
 #include <cstring>
 #include <fmt/base.h>
+#include <imgui.hpp>
 
 namespace ren {
 
 struct EditorContext {
   Arena m_arena;
+  Arena m_frame_arena;
   Renderer *m_renderer = nullptr;
   SDL_Window *m_window = nullptr;
   SwapChain *m_swap_chain = nullptr;
   Scene *m_scene = nullptr;
   ren::Handle<Camera> m_camera;
+  ImFont *m_font = nullptr;
+  bool m_quit = false;
 };
 
 void init_editor(int argc, const char *argv[], NotNull<EditorContext *> ctx) {
@@ -27,6 +32,7 @@ void init_editor(int argc, const char *argv[], NotNull<EditorContext *> ctx) {
   ScratchArena::init_allocator();
 
   ctx->m_arena = Arena::init();
+  ctx->m_frame_arena = Arena::init();
 
   {
     u32 adapter = DEFAULT_ADAPTER;
@@ -68,28 +74,112 @@ void init_editor(int argc, const char *argv[], NotNull<EditorContext *> ctx) {
 
   ctx->m_camera = create_camera(ctx->m_scene);
   set_camera(ctx->m_scene, ctx->m_camera);
+
+  if (!IMGUI_CHECKVERSION()) {
+    fmt::println(stderr, "Failed to check ImGui version");
+    exit(EXIT_FAILURE);
+  }
+
+  if (!ImGui::CreateContext()) {
+    fmt::println(stderr, "Failed to create ImGui context");
+    exit(EXIT_FAILURE);
+  }
+
+  ImGui::StyleColorsDark();
+
+  float display_scale = SDL_GetWindowDisplayScale(ctx->m_window);
+  float pixel_density = SDL_GetWindowPixelDensity(ctx->m_window);
+
+  ImGuiIO &io = ImGui::GetIO();
+  ImFont *default_font = io.Fonts->AddFontDefault();
+  ImFontConfig font_config = *find_if(
+      Span(io.Fonts->ConfigData), [&](const ImFontConfig &font_config) {
+        return font_config.DstFont == default_font;
+      });
+  font_config.FontDataOwnedByAtlas = false;
+  font_config.SizePixels = glm::floor(font_config.SizePixels * display_scale);
+  fill(ren::Span(font_config.Name), '\0');
+  font_config.DstFont = nullptr;
+  ctx->m_font = io.Fonts->AddFont(&font_config);
+  io.Fonts->Build();
+  io.FontGlobalScale = 1.0f / pixel_density;
+
+  ImGui::GetStyle().ScaleAllSizes(display_scale / pixel_density);
+
+  if (!ImGui_ImplSDL3_InitForVulkan(ctx->m_window)) {
+    fmt::println(stderr, "Failed to init ImGui backend");
+    exit(EXIT_FAILURE);
+  }
+
+  ren::init_imgui(&ctx->m_frame_arena, ctx->m_scene);
 };
 
+void draw_editor_ui(NotNull<EditorContext *> ctx) {
+  ImGui_ImplSDL3_NewFrame();
+  ImGui::NewFrame();
+  ImGui::PushFont(ctx->m_font);
+
+#if 0
+  bool open = true;
+  ImGui::ShowDemoWindow(&open);
+#endif
+  if (ImGui::BeginMainMenuBar()) {
+    if (ImGui::BeginMenu("File")) {
+      if (ImGui::MenuItem("Quit")) {
+        ctx->m_quit = true;
+      }
+      ImGui::EndMenu();
+    }
+    ImGui::EndMainMenuBar();
+  }
+
+  ImGui::PopFont();
+  ImGui::Render();
+  ImGui::EndFrame();
+}
+
 void run_editor(NotNull<EditorContext *> ctx) {
-  bool quit = false;
   u64 time = clock();
-  while (not quit) {
+  while (not ctx->m_quit) {
     u64 now = clock();
     u64 dt_ns = now - time;
     time = now;
 
-    SDL_Event e;
-    while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_EVENT_QUIT) {
-        quit = true;
+    ImGuiIO &io = ImGui::GetIO();
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      ImGui_ImplSDL3_ProcessEvent(&event);
+
+      switch (event.type) {
+      case SDL_EVENT_MOUSE_MOTION:
+      case SDL_EVENT_MOUSE_WHEEL:
+      case SDL_EVENT_MOUSE_BUTTON_DOWN:
+      case SDL_EVENT_MOUSE_BUTTON_UP:
+        if (io.WantCaptureMouse) {
+          continue;
+        }
+        break;
+      case SDL_EVENT_KEY_DOWN:
+      case SDL_EVENT_KEY_UP:
+        if (io.WantCaptureKeyboard) {
+          continue;
+        }
+        break;
+      case SDL_EVENT_QUIT:
+        ctx->m_quit = true;
+        break;
       }
     }
 
+    draw_editor_ui(ctx);
     ren::draw(ctx->m_scene, {.delta_time = dt_ns / 1e9f});
   }
 }
 
 void quit_editor(NotNull<EditorContext *> ctx) {
+  ImGui_ImplSDL3_Shutdown();
+  ImGui::DestroyContext();
   destroy_scene(ctx->m_scene);
   destroy_swap_chain(ctx->m_swap_chain);
   SDL_DestroyWindow(ctx->m_window);
