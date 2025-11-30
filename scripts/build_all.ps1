fafbce12798177131a21155dd3001e81931ff9ea
AdamFull <logotipick@gmail.com>
2025-11-30 23:32:14 +0100

    Build scripts for windows

---
+++ scripts/build_all.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = "Stop"
+$build="$PSScriptRoot/../build"
+
+cmake --build $build/msvc-dev
+if($LastExitCode -ne 0) { return }
+cmake --build $build/msvc-release
+if($LastExitCode -ne 0) { return }
+
+cmake --build $build/clang-dev
+if($LastExitCode -ne 0) { return }
+cmake --build $build/clang-release
