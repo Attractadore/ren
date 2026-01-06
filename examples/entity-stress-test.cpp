b51963b42dd43363078a9b8caa467f0ef4d02366
Michael Bargatin <attractadore02@gmail.com>
2026-01-06 02:03:04 +0100

    [examples]: convert entity stress test to gltf

--- examples/entity-stress-test.cpp
+++ examples/entity-stress-test.cpp
@@ -2,11 +2,10 @@
 #include "ren/baking/mesh.hpp"
 #include "ren/core/CmdLine.hpp"
 #include "ren/core/Format.hpp"
+#include "ren/core/GLTF.hpp"
+#include "ren/core/Job.hpp"
 #include "ren/core/sh/Random.h"
 
-#include <assimp/Importer.hpp>
-#include <assimp/postprocess.h>
-#include <assimp/scene.h>
 #include <cmath>
 #include <cstdlib>
 #include <fmt/base.h>
@@ -14,54 +13,88 @@
 
 namespace {
 
-ren::Handle<ren::Mesh> load_mesh(ren::NotNull<ren::Arena *> frame_arena,
-                                 ren::NotNull<ren::Scene *> scene,
-                                 ren::String8 path) {
+using namespace ren;
+
+struct DemoScene {
+  Span<glm::mat4x3> transforms;
+  Span<Handle<Mesh>> meshes;
+};
+
+DemoScene load_scene(ren::NotNull<ren::Arena *> frame_arena,
+                     ren::NotNull<ren::Scene *> scene, ren::Path path) {
   ren::ScratchArena scratch;
-  Assimp::Importer importer;
-  importer.SetPropertyBool(AI_CONFIG_PP_PTV_NORMALIZE, true);
-  const aiScene *ai_scene = importer.ReadFile(
-      path.zero_terminated(scratch),
-      // clang-format off
-      aiProcess_Triangulate |
-      aiProcess_GenNormals |
-      aiProcess_PreTransformVertices |
-      aiProcess_SortByPType |
-      aiProcess_FindInvalidData
-      // clang-format on
-  );
-  if (!ai_scene) {
-    fmt::println(stderr, "{}", importer.GetErrorString());
-    return ren::NullHandle;
+
+  ren::Result<ren::Gltf, ren::GltfErrorInfo> gltf =
+      ren::load_gltf_with_blobs(scratch, path);
+  if (!gltf) {
+    fmt::println(stderr, "{}", gltf.error().message);
+    exit(EXIT_FAILURE);
   }
-  assert(ai_scene->mNumMeshes > 0);
-  const aiMesh *ai_mesh = ai_scene->mMeshes[0];
-  assert(ai_mesh->mPrimitiveTypes & aiPrimitiveType_TRIANGLE);
-  assert(ai_mesh->HasPositions());
-  static_assert(sizeof(glm::vec3) == sizeof(*ai_mesh->mVertices));
-  assert(ai_mesh->HasNormals());
-  static_assert(sizeof(glm::vec3) == sizeof(*ai_mesh->mNormals));
-  static_assert(sizeof(glm::vec4) == sizeof(*ai_mesh->mColors[0]));
-  auto indices = ren::Span<ren::u32>::allocate(scratch, ai_mesh->mNumFaces * 3);
-  for (size_t f = 0; f < ai_mesh->mNumFaces; ++f) {
-    assert(ai_mesh->mFaces[f].mNumIndices == 3);
-    for (size_t i = 0; i < 3; ++i) {
-      indices[f * 3 + i] = ai_mesh->mFaces[f].mIndices[i];
+  // clang-format off
+  ren::gltf_optimize(scratch, &*gltf,
+      ren::GltfOptimize::RemoveCameras |
+      ren::GltfOptimize::RemoveMaterials |
+      ren::GltfOptimize::RemoveImages |
+      ren::GltfOptimize::RemoveSkins |
+      ren::GltfOptimize::RemoveAnimations |
+      ren::GltfOptimize::RemoveRedundantMeshes |
+      ren::GltfOptimize::ConvertMeshAccessors |
+      ren::GltfOptimize::CollapseSceneHierarchy |
+      ren::GltfOptimize::RemoveRedundantNodes |
+      ren::GltfOptimize::RemoveEmptyScenes | 
+      ren::GltfOptimize::NormalizeSceneBounds
+  );
+  // clang-format on
+  if (gltf->meshes.is_empty()) {
+    fmt::println(stderr, "Scene doesn't contain any (triangle) meshes");
+    exit(EXIT_FAILURE);
+  }
+
+  Span<usize> primitive_offsets =
+      Span<usize>::allocate(scratch, gltf->meshes.size());
+  usize num_primitives = 0;
+  for (usize mesh_index : range(gltf->meshes.size())) {
+    const GltfMesh &mesh = gltf->meshes[mesh_index];
+    primitive_offsets[mesh_index] = num_primitives;
+    num_primitives += mesh.primitives.size();
+  }
+
+  Span<Handle<Mesh>> primitive_handles =
+      Span<Handle<Mesh>>::allocate(scratch, num_primitives);
+  for (usize mesh_index : range(gltf->meshes.size())) {
+    const GltfMesh &mesh = gltf->meshes[mesh_index];
+    for (usize primitive_index : range(mesh.primitives.size())) {
+      ren::MeshInfo mesh_info = ren::gltf_primitive_to_mesh_info(
+          gltf->blobs[0], *gltf, mesh.primitives[primitive_index]);
+      ren::Blob blob = ren::bake_mesh_to_memory(scratch, mesh_info);
+      primitive_handles[primitive_offsets[mesh_index] + primitive_index] =
+          ren::create_mesh(frame_arena, scene, blob.data, blob.size);
     }
   }
-  ren::Blob blob = ren::bake_mesh_to_memory(
-      scratch,
-      {
-          .num_vertices = ai_mesh->mNumVertices,
-          .positions = reinterpret_cast<const glm::vec3 *>(ai_mesh->mVertices),
-          .normals = reinterpret_cast<const glm::vec3 *>(ai_mesh->mNormals),
-          .colors =
-              ai_mesh->HasVertexColors(0)
-                  ? reinterpret_cast<const glm::vec4 *>(ai_mesh->mColors[0])
-                  : nullptr,
-          .indices = indices,
-      });
-  return ren::create_mesh(frame_arena, scene, blob.data, blob.size);
+
+  DynamicArray<glm::mat4x3> scene_transforms;
+  DynamicArray<Handle<Mesh>> scene_meshes;
+  for (i32 node_index : gltf->scenes[0].nodes) {
+    const GltfNode &node = gltf->nodes[node_index];
+    if (node.mesh == -1) {
+      continue;
+    }
+    if (glm::transpose(node.matrix)[3] != glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)) {
+      continue;
+    }
+    const GltfMesh &mesh = gltf->meshes[node.mesh];
+    for (usize primitive_index : range(mesh.primitives.size())) {
+      scene_transforms.push(scratch, node.matrix);
+      scene_meshes.push(
+          scratch,
+          primitive_handles[primitive_offsets[node.mesh] + primitive_index]);
+    }
+  }
+
+  return {
+      .transforms = Span(scene_transforms).copy(frame_arena),
+      .meshes = Span(scene_meshes).copy(frame_arena),
+  };
 }
 
 glm::vec2 get_scene_bounds(unsigned num_entities) {
@@ -97,7 +130,7 @@ auto random_transform(float i, float min_trans, float max_trans,
 
 void place_entities(
     ren::NotNull<ren::Arena *> arena, ren::NotNull<ren::Arena *> frame_arena,
-    ren::Scene *scene, ren::Handle<ren::Mesh> mesh,
+    ren::Scene *scene, DemoScene demo_scene,
     ren::Handle<ren::Material> material, unsigned num_entities,
     ren::NotNull<ren::Handle<ren::MeshInstance> **> out_entities,
     ren::NotNull<glm::mat4x3 **> out_transforms) {
@@ -108,21 +141,26 @@ void place_entities(
 
   ren::ScratchArena scratch;
 
+  usize scene_size = demo_scene.transforms.size();
+
   auto *create_info =
-      scratch->allocate<ren::MeshInstanceCreateInfo>(num_entities);
-  auto *entities =
-      arena->allocate<ren::Handle<ren::MeshInstance>>(num_entities);
-  auto *transforms = arena->allocate<glm::mat4x3>(num_entities);
+      scratch->allocate<ren::MeshInstanceCreateInfo>(num_entities * scene_size);
+  auto *entities = arena->allocate<ren::Handle<ren::MeshInstance>>(
+      num_entities * scene_size);
+  auto *transforms = arena->allocate<glm::mat4x3>(num_entities * scene_size);
   for (size_t i = 0; i < num_entities; ++i) {
-    create_info[i] = {
-        .mesh = mesh,
-        .material = material,
-    };
-    transforms[i] =
+    glm::mat4x3 scene_transform =
         random_transform(i, bounds[0], bounds[1], min_scale, max_scale);
+    for (usize scene_index : range(scene_size)) {
+      create_info[i * scene_size + scene_index] = {
+          .mesh = demo_scene.meshes[scene_index], .material = material};
+      transforms[i * scene_size + scene_index] =
+          scene_transform * glm::mat4(demo_scene.transforms[scene_index]);
+    }
   }
-  ren::create_mesh_instances(frame_arena, scene, {create_info, num_entities},
-                             {entities, num_entities});
+  ren::create_mesh_instances(frame_arena, scene,
+                             {create_info, num_entities * scene_size},
+                             {entities, num_entities * scene_size});
   *out_entities = entities;
   *out_transforms = transforms;
 }
@@ -151,33 +189,36 @@ void set_camera(ren::Scene *scene, ren::Handle<ren::Camera> camera,
 
 class EntityStressTestApp : public ImGuiApp {
   ren::usize m_num_entities = 0;
+  usize m_scene_size = 0;
   ren::Handle<ren::MeshInstance> *m_entities = nullptr;
   glm::mat4x3 *m_transforms = nullptr;
 
 public:
-  void init(ren::String8 mesh_path, unsigned num_entities) {
+  void init(ren::Path mesh_path, unsigned num_entities) {
     ren::ScratchArena scratch;
     ImGuiApp::init(format(scratch, "Entity Stress Test: {} @ {}", mesh_path,
                           num_entities));
     m_num_entities = num_entities;
     ren::Scene *scene = get_scene();
     ren::Handle<ren::Camera> camera = get_camera();
-    ren::Handle<ren::Mesh> mesh = load_mesh(&m_frame_arena, scene, mesh_path);
+    DemoScene demo_scene = load_scene(&m_frame_arena, scene, mesh_path);
+    m_scene_size = demo_scene.transforms.size();
     ren::Handle<ren::Material> material =
         ren::create_material(&m_frame_arena, scene, {.metallic_factor = 0.0f});
-    place_entities(&m_arena, &m_frame_arena, scene, mesh, material,
+    place_entities(&m_arena, &m_frame_arena, scene, demo_scene, material,
                    m_num_entities, &m_entities, &m_transforms);
     place_light(scene);
     set_camera(scene, camera, num_entities);
   }
 
   void process_frame(ren::u64 dt_ns) override {
-    ren::set_mesh_instance_transforms(&m_frame_arena, get_scene(),
-                                      {m_entities, m_num_entities},
-                                      {m_transforms, m_num_entities});
+    ren::set_mesh_instance_transforms(
+        &m_frame_arena, get_scene(),
+        {m_entities, m_num_entities * m_scene_size},
+        {m_transforms, m_num_entities * m_scene_size});
   }
 
-  static void run(ren::String8 mesh_path, unsigned num_entities) {
+  static void run(ren::Path mesh_path, unsigned num_entities) {
     AppBase::run<EntityStressTestApp>(mesh_path, num_entities);
   }
 };
@@ -191,23 +232,25 @@ enum EntityStressTestOptions {
 
 int main(int argc, const char *argv[]) {
   ren::ScratchArena::init_for_thread();
+  ren::launch_job_server();
+  ren::ScratchArena scratch;
 
   // clang-format off
   ren::CmdLineOption options[] = {
-    {OPTION_FILE, ren::CmdLineString, "file", 'f', "Path to mesh", ren::CmdLinePositional},
+    {OPTION_FILE, ren::CmdLinePath, "file", 'f', "Path to mesh", ren::CmdLinePositional},
     {OPTION_NUM_ENTITIES, ren::CmdLineUInt, "num-entities", 'n', "Number of entities to draw"},
     {OPTION_HELP, ren::CmdLineFlag, "help", 'h', "Show this message"},
   };
   // clang-format on
   ren::ParsedCmdLineOption parsed[OPTION_COUNT];
-  bool success = ren::parse_cmd_line(argv, options, parsed);
+  bool success = ren::parse_cmd_line(scratch, argv, options, parsed);
   if (!success or parsed[OPTION_HELP].is_set) {
     ren::ScratchArena scratch;
     fmt::print("{}", ren::cmd_line_help(scratch, argv[0], options));
     return EXIT_FAILURE;
   }
 
-  ren::String8 mesh_path = parsed[OPTION_FILE].as_string;
+  ren::Path mesh_path = parsed[OPTION_FILE].as_path;
   ren::u32 num_entities = 100'000;
   if (parsed[OPTION_NUM_ENTITIES].is_set) {
     num_entities = parsed[OPTION_NUM_ENTITIES].as_uint;
